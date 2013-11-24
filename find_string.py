7f829fb4178f88c87d5041261547dbfbdcc67442
陆文举 <781624603@qq.com>
2013-11-24 11:36:43 +0800

    Create find_string.py

---
+++ find_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#-*-coding:UTF-8 -*-#
+#用法：./find_string.py 文件名称 字符串名称#
+#via: luwenju 2013-11-24
+
+import re
+import sys
+
+filepath=sys.argv[1]
+findstring=sys.argv[2]
+
+f = open(filepath, "r+")
+count=0
+for i in f.readlines():
+    li=re.findall(findstring, i)
+    if len(li) > 0:
+        count=count+1
+print count
+f.close()
+
