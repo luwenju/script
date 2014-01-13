0b407efea022fb35209695c94a80ebf2c6bca061
陆文举 <781624603@qq.com>
2014-01-13 15:50:14 +0800

    个人天气预报短信推送
    个人天气预报短信推送

---
+++ tianqi.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+#coding:gbk
+#20140113
+
+import urllib
+
+url="http://www.weather.com.cn/beijing/index.shtml"
+tel="13520200311"
+
+tmp=[]
+page=urllib.urlopen(url)
+for i in page.readlines()[443:448]:
+    a=i.strip()
+    b=a.lstrip('<td>')
+    test=b.rstrip('</td>')
+    tmp.append(test)
+page.close()
+tianqi=','.join(tmp)
+
+
+def SMS(x,y):
+    smsurl="http://ms.58control.cn/sms/warn?function=1000\&ms=%s&mo=%s" %(x,y)
+    ms=urllib.urlopen(smsurl)
+    ms.close()
+
+SMS(tianqi,tel)
